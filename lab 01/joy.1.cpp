d8615cf7df5c7505755c4f3784f4272d421f3381
Joy Gosh <140314137+Joy3865@users.noreply.github.com>
2023-07-30 15:06:40 +0600

    Add files via upload

---
+++ lab 01/joy.1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    cout<<"welcome to wonderful world of c++!!!!\n";
+    cout<<"lab 1\t";
+    cout<<"\nEnd\tPROGRAM\tBye";
+    cout<<"Welcome to youmark university\rHellow";
+    return 0;
+
+
+
+}
---
+++ lab 01/joy.2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    cout<<"Heloow in my university"<<endl;
+    cout<<endl;
+    cout<<"iam a studentofCSE Faculty"<<endl;
+    cout<<endl;
+
+    cout<<"welcome";
+}
